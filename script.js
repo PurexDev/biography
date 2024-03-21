dd8bffd5d421e9b91030c731264b9c0907356df0
PurexDev <Purex072@gmail.com>
2024-03-21 15:02:57 +0100

    Responsive
    Updated it to be a little bit more responsive. Not finished yet, but I'm getting somewhere :D.

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const splashText = document.getElementById("splashtext");
 const eventSplash = document.getElementById("splashscreen").addEventListener("click", splashScreen);
 
 // Music
+const music = document.getElementById("music");
 const audio = new Audio("audio/music.mp3");
+music.volume = 0.01;
 
 
 // Splashscreen function
