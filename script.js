d8c8844db51d00e797119577a2985127e9454078
PurexDev <Purex072@gmail.com>
2024-03-21 23:49:17 +0100

    Update
    Icons added, responsiveness better (still need to make text responsive) and location widget added.

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const eventSplash = document.getElementById("splashscreen").addEventListener("cl
 // Music
 const music = document.getElementById("music");
 const audio = new Audio("audio/music.mp3");
-music.volume = 0.01;
+music.volume = 0.1;
 
 
 // Splashscreen function
 function splashScreen(){
     splashscreen.style.opacity = 0;
+    music.play();
     setTimeout(function(){splashscreen.classList.remove("splashscreen");
     splashText.remove();}, 300);
-    music.play();
+    
 }
